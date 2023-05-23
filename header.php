703f1a632ee366aa96cde06df241119bdd1e637a
sapirowsky <sapirowsky@gmail.com>
2023-05-23 17:29:00 +0200

    upgrade

--- header.php
+++ header.php
@@ -9,41 +9,49 @@
 </head>
 <body <?php body_class() ?> >
 <div class="site">
-<header>
+<header class="header">
     <div class="container flex justify-between">
         <a href="<?php echo home_url() ?>" class="">
             <?php if ( has_custom_logo() ): ?>
                 <?php $image = wp_get_attachment_image_src( get_theme_mod('custom_logo'), 'full') ?>
-                <img src="<?php echo $image[0] ?>" class="logo" width="537px" height="132px" alt="CleverTouch logo">
+                <img src="<?php echo $image[0] ?>" class="logo" width="268,5px" height="66px" alt="CleverTouch logo">
             <?php else: ?>
-                <img src="<?php echo get_template_directory_uri() . "/assets/img/clevertouch-logo.webp" ?>" class="logo" width="537px" height="132px" alt="CleverTouch logo">
+                <img src="<?php echo get_template_directory_uri() . "/assets/img/clevertouch-logo.webp" ?>" class="logo" width="268,5px" height="66px" alt="CleverTouch logo">
             <?php endif ?>
         </a>
 
         <div class="controls flex">
-            <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')) ?>" class="account-container">
-                <img src="<?php echo get_template_directory_uri() . "/assets/img/user.svg" ?>" class="account-img" width="48px" height="48px" alt="account details">
+            <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')) ?>" class="icon">
+                <img src="<?php echo get_template_directory_uri() . "/assets/img/user.svg" ?>" style="padding: 2px;" width="48px" height="48px" alt="Konto">
             </a>
-            <div class="cart-container">
-                <label for="cart-checkbox" class="cart-icon">
-                    <img src="<?php echo get_template_directory_uri() . "/assets/img/cart.svg" ?>" class="cart-img" width="48px" height="48px" alt="Karta">
-
+            <div class="control-container">
+                <label for="cart-checkbox" class="icon">
+                    <img src="<?php echo get_template_directory_uri() . "/assets/img/cart.svg" ?>" width="48px" height="48px" alt="Koszyk">
                     <input id="cart-checkbox" type="checkbox">
                 </label>
-                <div class="cart-sidebar">
-                    <div class="close-container">
-                        <label for="cart-checkbox" class="close"></label>
+                <div class="sidebar">
+                    <div class="sidebar-close-container">
+                        <label for="cart-checkbox">
+                            <span class="sr-only">Zamknij nawigacje</span>
+                            X
+                        </label>
                     </div>
                     <h1>Lista zakupów</h1>
                 </div>
 
             </div>
-            <div class="sidebar-container">
-                <label class="hamburger-menu">
-                    <label class="sr-only" for="nav-checkbox">Nawigacja</label>
+            <div class="control-container">
+                <label for="nav-checkbox" class="sidebar-button flex">
+                    <span class="sr-only">Otwórz nawigacje</span>
                     <input id="nav-checkbox" type="checkbox" />
                 </label>
-                <nav class="nav-sidebar">
+                <nav class="sidebar">
+                    <div class="sidebar-close-container">
+                        <label for="nav-checkbox" class="sidebar-close">
+                            <span class="sr-only">Zamknij nawigacje</span>
+                            X
+                        </label>
+                    </div>
                     <?php 
                         wp_nav_menu([
                             'theme_location' => 'header-menu',
--- page.php
+++ page.php
@@ -1,9 +1,11 @@
 <?php get_header() ?>
 
-<main class="container">
-    <?php if(have_posts()): while(have_posts()): the_post()?>
-    <?php the_content() ?>
-    <?php endwhile; else: endif ?>
+<main class="flex justify-center">
+    <div class="container">
+        <?php if(have_posts()): while(have_posts()): the_post()?>
+        <?php the_content() ?>
+        <?php endwhile; else: endif ?>
+    </div>
 </main>
 
 <?php get_footer() ?>
